78c5c581b0e5dc264394e34f5628fcae3ca2dd1b
abdou idaala <abderrahmane@idaala.com>
2023-03-17 11:57:48 -0700

    commit task[4]

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Sccess)
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		switch(ch)
+		{
+		case 'e':
+			continue;
+		case 'q':
+			continue;
+		default:
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
