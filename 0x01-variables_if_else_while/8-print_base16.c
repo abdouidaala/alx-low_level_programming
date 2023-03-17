42daf32b10950c9f85ec8eeab932671daf7c6729
abdou idaala <abderrahmane@idaala.com>
2023-03-17 13:47:29 -0700

    commit task[8]

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * print all numbers of base 16
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 97; ch <= 102; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
