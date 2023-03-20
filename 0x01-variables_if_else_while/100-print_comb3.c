e63512a2bda8f5c4e5844d4b006b5aedfd3cc1d4
abdou idaala <abderrahmane@idaala.com>
2023-03-20 06:41:44 -0700

    update task[10]

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			if (i != j && i < j)
+			if (i < j)
 			{
 				putchar(i);
 				putchar(j);
-				if (i != 56)
+				if (i < 56)
 				{
 					putchar(44);
 					putchar(32);
