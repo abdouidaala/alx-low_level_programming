2ae85e2a0e46ba1f76e7f05481afce28a24e1406
abdou idaala <abderrahmane@idaala.com>
2023-03-21 16:09:45 -0700

    update task[3]

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,12 @@
 
 int _islower(int r)
 {
-	if (r != 0)
+	if (r >= 97 && r < 122)
 	{
-		_putchar('1');
+		return (1);
 	} else
 	{
-		_putchar('0');
+		return (0);
 	}
 	_putchar('\n');
-	return (0);
 }
