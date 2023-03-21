b7df81d238bd0abb43d9307d737baffc90b64c56
abdou idaala <abderrahmane@idaala.com>
2023-03-21 13:53:04 -0700

    update task[1]

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
   *
   * Return: Always 0 (Success)
   */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char ch;
 
@@ -14,5 +14,4 @@ int print_alphabet(void)
 		_putchar(ch);
 	}
 	_putchar('\n');
-	return (0);
 }
