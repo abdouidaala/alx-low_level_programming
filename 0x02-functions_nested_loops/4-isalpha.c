b0644067960fba9376b5de77bb24d92dde7343d6
abdou idaala <abderrahmane@idaala.com>
2023-03-21 16:44:20 -0700

    commit task[4]

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for Alphabetic Characters
+ *
+ * @r: Alphabetic character
+ *
+ * Return: 1 (Success) OR 0 (Failure)
+ */
+int _isalpha(int r)
+{
+	if (r >= 65 && r <= 90 || r >= 97 && r <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
