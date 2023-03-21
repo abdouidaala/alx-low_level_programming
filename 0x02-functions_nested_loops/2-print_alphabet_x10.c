9e938ab02af27222b242d870e08b976436876f8c
abdou idaala <abderrahmane@idaala.com>
2023-03-21 15:15:10 -0700

    update task[2]

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * print-alphabet - Entry point
-  *
-  * Return: alphabets a to z (10 times)
-  */
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: void
+ */
 void print_alphabet_x10(void)
 {
 	char ch;
