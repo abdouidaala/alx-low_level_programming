e628ab0df9c537b79e078e5ea094dc07e366deb0
abdou idaala <abderrahmane@idaala.com>
2023-03-21 14:01:56 -0700

    commit task[2]

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print-alphabet - Entry point
+  *
+  * Return: alphabets a to z (10 times)
+  */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+}
