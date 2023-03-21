45f2cc5c183aeaee6c9ceb685e5fb50f63ad7af2
Abdou Idaala <abderrahmane@idaala.com>
2023-03-21 19:22:40 +0000

    update task[0]

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
   */
 int main(void);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(ch);
+		ch++;
+	}
 	_putchar('\n');
 
 	return (0);
