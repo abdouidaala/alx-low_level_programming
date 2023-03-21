a6d4bdca6f22ce4713059bdc86245a9a31e63baf
abdou idaala <abderrahmane@idaala.com>
2023-03-21 14:05:32 -0700

    update task[2]

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,16 @@
 void print_alphabet_x10(void)
 {
 	char ch;
+	int i;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
-		_putchar('\n');
+		i = 1;
+		while (i <= 10)
+		{
+			_putchar('\n');
+			i++;
+		}
 	}
 }
