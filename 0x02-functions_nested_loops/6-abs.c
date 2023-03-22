e32d8c2b17cebf7fda89285db68067229292c865
abdou idaala <abderrahmane@idaala.com>
2023-03-21 17:15:40 -0700

    commit task[6]

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - COmputes the absolute value of an integer
+ *
+ *@r: The integer to be computed
+ *
+ * Return: Absolute value
+ */
+int _abs(int r)
+{
+	if (r >= 0 || r < 0)
+	{
+		_putchar(r);
+	}
+	return (0);
+}
