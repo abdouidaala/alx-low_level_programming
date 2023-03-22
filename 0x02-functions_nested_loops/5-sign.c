0e92b6c86c486613f74041cb2cd1384c9eb97607
abdou idaala <abderrahmane@idaala.com>
2023-03-21 17:03:16 -0700

    commit task[5]

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of a number
+ *
+ * @r: Number
+ *
+ * Return: 1 (is greater that zero) OR 0 (is zero)
+ * OR -1 (is less that zero)
+ */
+int int print_sign(int r)
+{
+	if (r > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (r == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
