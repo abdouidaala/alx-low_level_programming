da2649913358d5790d5cecc70371eb3c9000c033
abdou idaala <abderrahmane@idaala.com>
2023-03-21 18:42:33 -0700

    commit task[7]

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @r: A number
+ *
+ * Return: Last digit of n
+ */
+int print_last_digit(int r)
+{
+	return (r % 10 + '0');
+}
