8266a6b14d75c2921f9c2b3080a800e797b7341f
abdou idaala <abderrahmane@idaala.com>
2023-03-22 09:14:25 -0700

    update task[9]

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,10 +11,14 @@ void times_table(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		a = (i * j) % 10 + '0';
 		for (j = '0'; j <= '9'; j++)
 		{
-			_putchar(a);
+			a = (i * j);
+			if (a >= '9')
+			{
+				_putchar(a / 10);
+			}
+			_putchar(a % 10);
 			if (j < '9')
 			{
 				_putchar(44);
