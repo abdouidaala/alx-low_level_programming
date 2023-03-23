149d88ea6a6a744a3da7558658dea6100fb95432
abdou idaala <abderrahmane@idaala.com>
2023-03-23 05:02:36 -0700

    update task[7]

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,19 +9,16 @@
  */
 void print_diagonal(int n)
 {
-	if (n > 0)
+	if (n <= 0)
 	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
 		_putchar('\n');
 	}
 	else
 	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(92);
+		}
 		_putchar('\n');
 	}
 }
