34ec12474cbb3bb524fe92c0c2b3e6d92b52fe32
abdou idaala <abderrahmane@idaala.com>
2023-03-23 02:28:19 -0700

    task[2]

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiply to integers
+ *
+ * @a: interger one
+ * @b: interger two
+ *
+ * Return: Proudct of a and b
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
