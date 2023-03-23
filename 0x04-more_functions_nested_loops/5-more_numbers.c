827691c618a9e0a8bf8f9ef81f7dcba80a1f3f7d
abdou idaala <abderrahmane@idaala.com>
2023-03-23 03:32:15 -0700

    task[5]

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print 10x the numbers 0-14
+ *
+ * Return: 10x the numbers 0-14
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
