980edb7717561f7cc3576132d7e55248bf86979c
abdou idaala <abderrahmane@idaala.com>
2023-03-23 03:05:29 -0700

    task[4]

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints 0-9 leaving out 2 and 4.
+ *
+ * Return: 0-9
+ *
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		switch (i)
+		{
+			case 50:
+				continue;
+			case 52:
+				continue;
+		}
+		_putchar(i++);
+	}
+	_putchar('\n');
+}
