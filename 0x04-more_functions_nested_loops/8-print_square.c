2238a07e395cb61cd67d8633f7093175c951e847
abdou idaala <abderrahmane@idaala.com>
2023-03-23 16:08:07 -0700

    update task[8]

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,13 @@
  */
 void print_square(int size)
 {
-	int i, j;
-
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+	int i, j;
 		for (i = 0; i <= size; i++)
 		{
 			for (j = 0; j <= size; j++)
