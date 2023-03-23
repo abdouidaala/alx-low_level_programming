e4a71f1f0a9c7af0207cdd9caec53d157a403ec1
abdou idaala <abderrahmane@idaala.com>
2023-03-23 13:03:50 -0700

    task[8]

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Prints n square according n number of times
+ *
+ * @size: The number of squares/number of times
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
