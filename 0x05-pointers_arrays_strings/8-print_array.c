98f6a70f4019ee3e9d93bdd77825bb29c597ecbf
abdou idaala <abderrahmane@idaala.com>
2023-03-28 08:52:51 -0700

    task[8]

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+}
