86668d41bb5d09a4df206854f137234b45ef5c52
abdou idaala <abderrahmane@idaala.com>
2023-03-28 02:18:36 -0700

    task[2]

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: The character containing the string
+ *
+ * Return: The length of the character
+ */
 int _strlen(char *s)
 {
 	int len;
