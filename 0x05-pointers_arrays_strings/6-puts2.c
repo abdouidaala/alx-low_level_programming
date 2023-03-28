e41c4013f203979999d0a3fc67d767d20e5553f4
abdou idaala <abderrahmane@idaala.com>
2023-03-28 07:54:14 -0700

    task[6]

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: Input string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
