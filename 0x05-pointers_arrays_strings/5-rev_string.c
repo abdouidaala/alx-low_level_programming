35dcca7670b29f1dd81939c99719f663f19a2be2
abdou idaala <abderrahmane@idaala.com>
2023-03-28 07:20:47 -0700

    task[5]

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,16 @@
 #include "main.h"
 #include <string.h>
 
-void rev_string(char *s)
-{
-	_strrev(s);
-	_putchar('\n');
+void rev_string(char* s) {
+	int i, len;
+
+	len = strlen(s);
+	for (i = 0; i < len / 2; i++)
+	{
+		char c;
+		c = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = c;
+	}
+	putchar('\n');
 }
