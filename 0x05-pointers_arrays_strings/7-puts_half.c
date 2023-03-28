3d36bf1748f4d3564c43d53331b6057f7253f0ba
abdou idaala <abderrahmane@idaala.com>
2023-03-28 08:39:12 -0700

    task[7]

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,19 +16,19 @@ void puts_half(char *str)
 	len = strlen(str);
 	if ((len % 2) == 0)
 	{
-        	n = len / 2;
-        	for (i = n; i < len; i++)
-        	{
-            		_putchar(str[i]);
-        	}
-    	}
-        else
-        {
-        	n = (len / 2) + 1;
-        	for (i = n; i < len; i++)
-        	{
-            		_putchar(str[i]);
-        	}
-        }
+		n = len / 2;
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = (len / 2) + 1;
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
 	_putchar('\n');
 }
