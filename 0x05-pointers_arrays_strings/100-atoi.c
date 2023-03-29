78586f84aeccd76a6989918bb81c78c1dda44796
abdou idaala <abderrahmane@idaala.com>
2023-03-28 19:52:26 -0700

    task[10]

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _atoi - convert a number in a string to an integer
+ *
+ * @s: String to be converted
+ *
+ * Return: converted integer OR 0 if none
+ */
 int _atoi(char *s)
 {
 	int n = atoi(s);
