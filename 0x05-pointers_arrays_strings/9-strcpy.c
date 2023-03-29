0f5566134d3b22847111bf860cdcc7cf53de18fa
abdou idaala <abderrahmane@idaala.com>
2023-03-28 19:30:18 -0700

    task[9]

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
-char* _strcpy(char* dest, char* src)
+/**
+ * _strcpy - Copy strings from source arr to destination arr
+ *
+ * @dest: destination array
+ * @src: source array
+ *
+ * Return: Pointer to destination array
+ */
+char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
 	return (dest);
