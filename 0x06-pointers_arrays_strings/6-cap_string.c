5208439ca510036a20a55805fef3512e5822d7e1
abdou idaala <abderrahmane@idaala.com>
2023-03-30 09:17:35 -0700

    task[6]

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,8 @@ char *cap_string(char *str)
 
 	for (i = 0; i < len; i++)
 	{
-		if ((str[i - 1] == ' ') || (str[i - 1] == '\n') || (str[i - 1] == '\t'))
+		if ((str[i - 1] == ' ') || (str[i - 1] == '\n') || (str[i - 1] == '\t')
+		|| (str[i - 1] == '.'))
 		{
 			str[i] = toupper(str[i]);
 		}
