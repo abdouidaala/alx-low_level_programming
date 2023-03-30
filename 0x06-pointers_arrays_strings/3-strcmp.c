b9bfa6bc4b50354315b35342ffc97abe90f17914
abdou idaala <abderrahmane@idaala.com>
2023-03-29 21:24:03 -0700

    task[3]

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,14 @@
 #include <string.h>
 
+/**
+ * _strcmp - Compare two strings
+ *
+ * @s1: First string
+ *
+ * @s2: Second string
+ *
+ * Return: 0 if equal, less than 1 or greater than 1
+ */
 int _strcmp(char *s1, char *s2)
 {
 	return (strcmp(s1, s2));
