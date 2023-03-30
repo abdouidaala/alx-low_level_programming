20e43a97a9b1aef62e403c09e079f3277fa565b6
abdou idaala <abderrahmane@idaala.com>
2023-03-30 08:07:54 -0700

    task[6]

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,9 +16,34 @@ char *cap_string(char *str)
 
 	for (i = 0; i < len; i++)
 	{
-		if (isspace(str[i - 1]) || ispunct(str[i - 1]))
+		if (isspace(str[i - 1])
 		{
 			str[i] = toupper(str[i]);
+		}
+		switch (str[i - 1])
+		{
+			case 33:
+				str[i] = toupper(str[i]);
+			case 34:
+				str[i] = toupper(str[i]);
+			case 40:
+				str[i] = toupper(str[i]);
+			case 41:
+				str[i] = toupper(str[i]);
+			case 44:
+				str[i] = toupper(str[i]);
+			case 46:
+				str[i] = toupper(str[i]);
+			case 59:
+				str[i] = toupper(str[i]);
+			case 63:
+				str[i] = toupper(str[i]);
+			case 123:
+				str[i] = toupper(str[i]);
+			case 125:
+				str[i] = toupper(str[i]);
+			default:
+				break;
 		}
 	}
 
