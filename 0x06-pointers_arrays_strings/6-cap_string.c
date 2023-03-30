89b0001666ec672520711e5dae928d68ceb320a7
abdou idaala <abderrahmane@idaala.com>
2023-03-30 07:10:04 -0700

    task[6]

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @str: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (isspace(str[i - 1]) || ispunct(str[i - 1]))
+		{
+			str[i] = toupper(str[i]);
+		}
+	}
+
+	return (str);
+}
