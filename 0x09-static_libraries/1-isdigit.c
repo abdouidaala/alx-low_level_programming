a7d65a00eb3c1843644b93dd09c848a9e163513b
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:10:35 -0700

    update static library

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for numbers
+ *
+ * @c: character to checked
+ *
+ * Return: 1 OR 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
