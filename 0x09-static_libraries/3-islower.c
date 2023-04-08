b23a0948a506cb06e5a202a0495870d458140f90
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:31:42 -0700

    update static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ *
+ * @r: Number to be checked
+ *
+ * Return: 1 (Success) OR 0 (Failure)
+ */
+int _islower(int r)
+{
+	if (r >= 97 && r <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
