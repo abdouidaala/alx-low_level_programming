5c594cdf04f25cf26e330676fe2fcc69c9e7e330
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:46:15 -0700

    update static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - COmputes the absolute value of an integer
+ *
+ *@r: The integer to be computed
+ *
+ * Return: Absolute value
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r *= -1;
+		return (r);
+	}
+	else
+	{
+		return (r);
+	}
+}
