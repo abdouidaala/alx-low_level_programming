3cc80710f0aca7c957ca4451c4cf14b99cef1579
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:27:26 -0700

    update static library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: The character containing the string
+ *
+ * Return: The length of the character
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
