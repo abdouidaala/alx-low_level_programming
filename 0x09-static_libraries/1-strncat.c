edc96b14ac472e1ddda0f7c3ad1ea8d882cc5dcb
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:16:15 -0700

    update static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - Concatenates two strings using @ bytes
+ *
+ * @dest: Destination String; src string is appended to it
+ *
+ * @src: Source string; gets appended to dest string
+ *
+ * @n: Number of bytes of src that should be concatenated with dest
+ *
+ * Return: Pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
