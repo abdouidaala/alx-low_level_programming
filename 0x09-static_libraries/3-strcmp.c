a4c53fc404b7cc743c8153c5eaa94dbc3389f28f
abdou idaala <abderrahmane@idaala.com>
2023-04-08 08:35:48 -0700

    update static library

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compare two strings
+ *
+ * @s1: First string
+ *
+ * @s2: Second string
+ *
+ * Return: 0 if equal, less than 1 or greater than 1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
