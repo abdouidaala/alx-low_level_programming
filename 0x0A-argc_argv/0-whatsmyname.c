352e0717ce6339e3b78dc43c269981f1ba6670ce
abdou idaala <abderrahmane@idaala.com>
2023-04-09 20:31:25 -0700

    task[0]

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	puts(argv[argc - 1]);
