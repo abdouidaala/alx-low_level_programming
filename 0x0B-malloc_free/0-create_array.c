e47f8d3b11580811ac346f7f5e6a159014488003
abdou idaala <abderrahmane@idaala.com>
2023-04-11 02:50:40 -0700

    task[0]

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    else
+    {
+        int i, *p;
+
+        p = malloc(size * sizeof(c));
+
+        if (p == NULL)
+            return (NULL);
+        
+        for (i = 0; i < size; i++)
+        {
+            p[i] = c;
+            return (p);
+        }
+    }
+    return (0);
+}
