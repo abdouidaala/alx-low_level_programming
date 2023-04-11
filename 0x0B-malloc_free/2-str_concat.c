c072daa54e06104f81a6f99c35a9fe77c7fa014e
abdou idaala <abderrahmane@idaala.com>
2023-04-11 16:33:46 -0700

    task[2]

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,15 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *str = malloc(strlen(s1) + strlen(s2) + 1);
+	char *str;
 	int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	str = malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (str == NULL)
 		return (NULL);
