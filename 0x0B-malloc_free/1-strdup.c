a33eec773960ffc5393278a6befa9e4f129f56fb
abdou idaala <abderrahmane@idaala.com>
2023-04-11 08:22:25 -0700

    task[1]

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strdup - returns a pointer to a copy of string
+ *
+ * @str:string parameter
+ *
+ * Return:NULL(fail)
+ */
 char *_strdup(char *str)
 {
 	char *a;
