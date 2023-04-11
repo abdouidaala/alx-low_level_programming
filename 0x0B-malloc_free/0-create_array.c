76d5404a61fa3dee124de946b22369482e8046ce
abdou idaala <abderrahmane@idaala.com>
2023-04-11 07:15:56 -0700

    task[0]

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *p = malloc(size * sizeof(c));
 
 	if (p == NULL || size == 0)
 		return (NULL);
  
-	unsigned int i;
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
