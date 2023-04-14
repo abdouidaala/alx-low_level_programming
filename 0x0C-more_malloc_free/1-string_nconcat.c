c0ea0873e92f357a667a7fe917b0e17b467265d1
abdou idaala <abderrahmane@idaala.com>
2023-04-13 19:18:25 -0700

    task[1]

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j;
+	char *p = malloc(strlen(s1) + n);
+
+	for (i = 0; i < strlen(s1); i++)
+		p[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+
+	p[i] = '\0';
+	return (p);
+}
