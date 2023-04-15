844b10a59a8c9756f86f2f793730136643587c5a
abdou idaala <abderrahmane@idaala.com>
2023-04-15 07:53:22 -0700

    task[2]

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,10 +4,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr = malloc(nmemb * size);
+	int i;
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
 
 	if (ptr == NULL)
 		return (NULL);
-	
-	free(ptr);
+
 	return (ptr);
 }
