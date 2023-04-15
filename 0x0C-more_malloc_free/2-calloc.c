26319638249fb3edcce9c6b0e23b00a2fcd3770d
abdou idaala <abderrahmane@idaala.com>
2023-04-15 08:00:37 -0700

    task[2]

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr = malloc(nmemb * size);
-	int i;
 
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+	memset(ptr, 0, nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
