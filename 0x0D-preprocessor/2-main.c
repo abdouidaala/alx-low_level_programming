ea9575d7b70b36d9bcb6cb0569edd6bbaaf66b1f
abdou idaala <abderrahmane@idaala.com>
2023-04-17 15:53:45 -0700

    task[2]

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char *ptr = __FILE__;
@@ -10,5 +16,6 @@ int main(void)
 		putchar(ptr[i]);
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
