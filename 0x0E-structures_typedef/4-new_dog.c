d60acc353976463501f75d4a83fddefc81b5c9b1
abdou idaala <abderrahmane@idaala.com>
2023-04-17 23:47:14 -0700

    task[4]

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,18 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * new_dog - Creates a new dog
+ *
+ * @name: Name of dog
+ *
+ * @age: Age of dog
+ *
+ * @owner: Owner of dog
+ *
+ * Return: Pointer to the newly created dog (SUCCESS) or
+ * NULL if insufficient memory was available (FAILURE)
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p = malloc(sizeof(dog_t));
