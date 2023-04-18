8946c1026b19bd122c9eb584b41fa1cf6a4e5e18
abdou idaala <abderrahmane@idaala.com>
2023-04-17 19:23:21 -0700

    task[1]

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,11 +4,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(strlen(name));
+	d->name = malloc(strlen(name) + );
 	strcpy(d->name, name);
 
 	d->age = age;
 
-	d->owner = malloc(strlen(owner));
+	d->owner = malloc(strlen(owner) + 1);
 	strcpy(d->owner, owner);
 }
