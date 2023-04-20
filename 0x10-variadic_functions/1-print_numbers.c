272a39b35d66a464ecf0ce45297c5377495ab287
abdou idaala <abderrahmane@idaala.com>
2023-04-20 09:10:29 -0700

    task[1]

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (separator == NULL)
-		exit(1);
+		return;
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
