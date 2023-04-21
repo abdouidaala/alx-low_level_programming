df0063ecb4379a6f7f7aa95f97dd92ccc3bb5381
abdou idaala <abderrahmane@idaala.com>
2023-04-21 04:01:44 -0700

    task[3]

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 				if (va_arg(args, char *) == NULL)
 				{
 					printf("(nil)");
-					break;
+					return;
 				}
 				printf("%s", va_arg(args, char *));
 				break;
