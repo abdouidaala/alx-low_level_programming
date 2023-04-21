66a04e9baea9d59722a8a388c3f0264ed9b05a0b
abdou idaala <abderrahmane@idaala.com>
2023-04-20 22:29:16 -0700

    task[3]

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,31 +9,31 @@
  */
 void print_all(const char * const format, ...)
 {
-        va_list args;
-        int len = strlen(format);
-        int i;
+	va_list args;
+	int len = strlen(format);
+	int i;
 
-        va_start(args, format);
-        i = 0;
-        while (i < len)
-        {
-                switch (format[i])
-                {
-                        case 'i':
-                                printf("%d", va_arg(args, int));
-                                break;
-                        case 'f':
-                                printf("%f", va_arg(args, double));
-                                break;
-                        case 'c':
-                                printf("%c", va_arg(args, int));
-                                break;
-                        case 's':
-                                printf("%s", va_arg(args, char *));
-                                break;
-                }
-                i++;
-        }
-        va_end(args);
-        printf("\n");
+	va_start(args, format);
+	i = 0;
+	while (i < len)
+	{
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				printf("%s", va_arg(args, char *));
+				break;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
