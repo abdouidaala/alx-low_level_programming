dbdbdbcf7453b333fd4a9abb799b3099a9e67f94
abdou idaala <abderrahmane@idaala.com>
2023-04-21 10:37:24 -0700

    task[3]

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,7 +40,7 @@ void print_all(const char * const format, ...)
 				sep = 1;
 				break;
 		}
-		if (i < (strlen(format) - 1) && sep != 1)
+		while (i < (strlen(format) - 1) && sep != 1)
 			printf(", ");
 		i++;
 	}
