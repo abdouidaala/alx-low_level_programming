7a59a18d457e5dd8e496b272573d4aa914af03e8
abdou idaala <abderrahmane@idaala.com>
2023-04-21 23:16:41 -0700

    task[2]

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -23,7 +23,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	for (i = 0; i < size; i++)
-		if (cmp(array[i]) || array[i] != 98)
+		if (cmp(array[i]) && array[i] != 98)
 			return (i);
 	return (0);
 }
