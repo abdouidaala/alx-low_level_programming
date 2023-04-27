f801f3b591b60019b2be7d9ba2f47d1bc8dfac18
abdou idaala <abderrahmane@idaala.com>
2023-04-27 15:12:13 -0700

    task[2]

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,5 +2,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	unsigned int length;
+	list_t *ptr = malloc(sizeof(list_t));
+
+	length = strlen(str);
+
+	if (!ptr)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = length;
+	ptr->next = *head;
+
+	*head = ptr;
+
+	return (ptr);
 }
