7afd6357cc274411e86fa76a2eead890c08e9561
abdou idaala <abderrahmane@idaala.com>
2023-04-27 05:34:35 -0700

    task[1]

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,11 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
