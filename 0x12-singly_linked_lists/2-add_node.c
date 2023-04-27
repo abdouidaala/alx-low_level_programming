2486b91ca5fe69d4365b3cc712c647cfb63543bc
abdou idaala <abderrahmane@idaala.com>
2023-04-27 15:14:53 -0700

    task[2]

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * add_node - add a new node at beginning of a list_t list.
+ * @head: head of a list_t list.
+ * @str: value to insert into element.
+ * Return: the number of nodes.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int length;
