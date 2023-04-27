9993b84cb13881f7804ea5a16fd478319e4e4661
abdou idaala <abderrahmane@idaala.com>
2023-04-27 04:52:55 -0700

    task[0]

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,8 @@ size_t print_list(const list_t *h)
 
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
-	printf("[%d] %s\n", h->len, h->str);
+	else
+		printf("[%d] %s\n", h->len, h->str);
 	printf("[%d] %s\n", (h->next)->len, (h->next)->str);
 
 	while (h != NULL)
