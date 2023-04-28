128f53da0e59d7bee42b1b90cd2e0b1b9288a4af
abdou idaala <abderrahmane@idaala.com>
2023-04-27 17:34:12 -0700

    task[4]

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_list - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
 void free_list(list_t *head)
 {
 	list_t *ptr = malloc(sizeof(list_t));
