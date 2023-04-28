ccf1f38f5e5d6304f53888a2fe1f31068d0a9a18
abdou idaala <abderrahmane@idaala.com>
2023-04-27 17:10:06 -0700

    task[3]

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int length;
+	list_t *ptr, *tmp;
+
+	length = strlen(str);
+	tmp = malloc(sizeof(list_t));
+
+	if (!tmp)
+		return (NULL);
+
+	tmp->str = strdup(str);
+	tmp->len = length;
+	tmp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (tmp);
+	}
+
+	*head = ptr;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = tmp;
+
+	return (tmp);
+}
