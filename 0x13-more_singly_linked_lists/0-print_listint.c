fad3534633a98364b2a0faaa2cfef1011bc3a9f1
abdou idaala <abderrahmane@idaala.com>
2023-04-29 10:07:32 -0700

    task[0]

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ *
+ * @h: head of linklist node
+ *
+ * Return: the number of nodes
+ *
+ */
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
