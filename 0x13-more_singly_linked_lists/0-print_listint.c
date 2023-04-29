d99030318f96d92d72a622dc8c32ff850f9e18e0
abdou idaala <abderrahmane@idaala.com>
2023-04-29 10:06:14 -0700

    task[0]

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,9 +5,10 @@ size_t print_listint(const listint_t *h)
 	int count = 0;
 	const listint_t *ptr = NULL;
 
-	if (h == NULL)
-		return (0);
 	for (ptr = h; ptr != NULL; ptr = ptr->next)
+	{
+		printf("%d\n", ptr->n);
 		count++;
+	}
 	return (count);
 }
