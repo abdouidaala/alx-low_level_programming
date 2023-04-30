9f58a589d47e70e4b6a8313be8f065c869ca113b
abdou idaala <abderrahmane@idaala.com>
2023-04-30 02:53:30 -0700

    task[4]

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: the head of the list
+ *
+ * Return: NULL if error
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *ptr, *tmp;
