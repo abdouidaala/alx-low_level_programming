2c40f381fdf31e2457d681063e2feefec15b921a
abdou idaala <abderrahmane@idaala.com>
2023-04-30 03:01:02 -0700

    task[5]

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: a double pointer of lists
+ *
+ * Return: void
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr, *tmp;
