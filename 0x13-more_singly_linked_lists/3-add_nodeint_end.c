1f262d13e65d9b9b3270453fb2ab089814959802
abdou idaala <abderrahmane@idaala.com>
2023-04-30 00:20:57 -0700

    task[3]

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,5 +2,21 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *ptr, *tmp;
 
+	tmp = malloc(sizeof(listint_t));
+	tmp->n = n;
+	tmp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (0);
+	}
+
+	ptr = *head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = tmp;
+	return (tmp);
 }
