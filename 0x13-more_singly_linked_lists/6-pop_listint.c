5be802700feea72688dbd6307708d5cc5bac1cbd
abdou idaala <abderrahmane@idaala.com>
2023-04-30 11:46:59 -0700

    task[6]

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,21 +8,13 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *ptr;
 	int data = 0;
 
 	if (*head == NULL)
 		return (0);
 	if ((*head)->next == NULL)
 		data = (*head)->n;
-	ptr = *head;
-	while (ptr)
-	{
-		ptr = ptr->next;
-		free(*head);
-		*head = ptr;
-	}
-	free(ptr);
+	free(*head);
 	*head = NULL;
 	return (data);
 }
