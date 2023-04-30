c79b7fc660e427708c436644c9fd04dfb1de86b6
abdou idaala <abderrahmane@idaala.com>
2023-04-30 03:28:41 -0700

    task[5]

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head)
+	while (*head)
 	{
 		ptr = *head;
-
-		while (ptr)
-		{
-			ptr = ptr->next;
-			free(*head);
-			*head = ptr;
-		}
+		free(ptr);
+		*head = ptr->next;
 	}
-	*head = NULL;
 }
