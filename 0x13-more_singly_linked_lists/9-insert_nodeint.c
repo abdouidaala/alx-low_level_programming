dfd074c7bbc599b9b758b28cac9aa41f3d4ca9dc
abdou idaala <abderrahmane@idaala.com>
2023-05-01 12:17:21 -0700

    task[9]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = ptr->next;
 			ptr->next = new;
-			return (ptr);
+			return (new);
 		}
 		ptr = ptr->next;
 		i++;
