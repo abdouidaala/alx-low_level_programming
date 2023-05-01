24d9c4c8b7e648c801432958cf81ac6403061bd3
abdou idaala <abderrahmane@idaala.com>
2023-05-01 07:12:56 -0700

    task[9]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - inserting a new node at agiven postion
+ * @head: double pointer
+ * @idx: index of the node
+ * @n: new node value
+ *
+ * Return: the address of new node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr;
