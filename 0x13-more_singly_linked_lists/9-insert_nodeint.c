5514beac6eb6513d3d6084807560396960f2a660
abdou idaala <abderrahmane@idaala.com>
2023-05-01 07:47:33 -0700

    task[9]

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			ptr->n = n;
-			ptr->next = NULL;
+			ptr->next = *head;
 			return (ptr);
 		}
 		*head = (*head)->next;
