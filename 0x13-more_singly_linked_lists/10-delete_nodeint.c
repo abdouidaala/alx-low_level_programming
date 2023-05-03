4613b69fd29b02ee620bcb941b6e3ffbfd97fa5f
abdou idaala <abderrahmane@idaala.com>
2023-05-03 00:05:12 -0700

    task[10]

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,9 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (*head)
 	{
-		if (index > ++i)
-			return (-1);
-		if (i == index)
+		if (++i == index)
 		{
 			curr = prev->next;
 			prev->next = curr->next;
@@ -33,6 +31,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			curr = NULL;
 			return (1);
 		}
+		else if (i < index)
+			return (-1);
 		prev = prev->next;
 	}
 	return (-1);
