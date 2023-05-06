d68508bf2c1b18cc5d82ff079e1678a66d8ee996
abdou idaala <abderrahmane@idaala.com>
2023-05-05 17:40:13 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number
@@ -16,9 +17,9 @@ void print_binary(unsigned long int n)
 	while (result)
 	{
 		if (result & n)
-			_putchar('1');
+			printf("%c", '1');
 		else
-			_putchar('0');
+			printf("%c", '0');
 
 		result >>= 1;
 	}
