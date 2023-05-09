2fc82a436f362acb2bd8eca8dea13028b99374d5
abdou idaala <abderrahmane@idaala.com>
2023-05-08 19:02:32 -0700

    task[2]

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,14 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND);
+	write(1, text_content, sizeof(text_content));
+	if (fd == -1)
+		return (-1);
+	close(fd);
 	return (1);
 }
