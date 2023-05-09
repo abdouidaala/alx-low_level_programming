fc53c5cf8b86e25c135c50d71e855d246e3abc6a
abdou idaala <abderrahmane@idaala.com>
2023-05-08 18:19:50 -0700

    task[0]

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	bytes = read(fd, buffer, letters);
 	bytes = write(1, buffer, bytes);
 	if (fd == -1)
