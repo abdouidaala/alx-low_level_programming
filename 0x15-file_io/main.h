f85da0ca040d69780103415d5926e122b3652ea3
abdou idaala <abderrahmane@idaala.com>
2023-05-08 18:00:29 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
