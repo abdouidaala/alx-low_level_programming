9eeb76986cefad88de676a209a40c9939eb83b98
abdou idaala <abderrahmane@idaala.com>
2023-05-08 18:35:37 -0700

    task[1]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT);
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 	write(1, text_content, sizeof(text_content));
 	if (fd == -1)
 		return (-1);
