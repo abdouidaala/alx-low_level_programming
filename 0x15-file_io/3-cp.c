4644ebc874894e3e80fd29444c49b8a2435e8371
abdou idaala <abderrahmane@idaala.com>
2023-05-09 16:43:47 -0700

    task[3]

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
+void *exit_97(void)
+{
+    char *msg = "Usage: cp file_from file_to\n";
+    int len = 0;
+
+    if (msg)
+    {
+        while (msg[len])
+            len++;
+    }
+    write(STDERR_FILENO, msg, len);
+    exit(97);
+}
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 
 	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 1024);
-		exit(97);
-	}
+		exit_97();
 	return (0);
 }
