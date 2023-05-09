156d3f30bb441e60d44279fdf72cbacb6d8cd347
abdou idaala <abderrahmane@idaala.com>
2023-05-09 00:31:06 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	r = read(fd, buffer, letters);
-	w = write(1, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 	free(buffer);
 	close(fd);
 	return (w);
