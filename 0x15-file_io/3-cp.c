31ea02bf3ef30098fad3572d08a19f407c838828
abdou idaala <abderrahmane@idaala.com>
2023-05-10 00:29:26 -0700

    task[3]

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,42 +19,23 @@ int main(int argc, char *argv[])
 	ssize_t r;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, ARGUMENTS_FAILS);
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, ARGUMENTS_FAILS), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, CAN_NOT_READ, argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, CAN_NOT_READ, argv[1]), exit(98);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, CAN_NOT_WRITE, argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, CAN_NOT_WRITE, argv[2]), exit(99);
 	while ((r = read(file_from, buffer, 1024)) > 0)
 	{
 		if (write(file_to, buffer, r) < 0)
-		{
-			dprintf(STDERR_FILENO, CAN_NOT_WRITE, argv[2]);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, CAN_NOT_WRITE, argv[2]), exit(99);
 	}
 	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, CAN_NOT_READ, argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, CAN_NOT_READ, argv[1]), exit(98);
 	close_from = close(file_from);
 	close_to = close(file_to);
 	if ((close_from == -1) || (close_to == -1))
-	{
-		dprintf(STDERR_FILENO, CAN_NOT_CLOSE, file_from);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, CAN_NOT_CLOSE, file_from), exit(100);
 	return (0);
 }
