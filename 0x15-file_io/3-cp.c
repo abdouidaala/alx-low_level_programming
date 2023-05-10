ee4c039dd66f80b5625da094aa1c788756cb5339
abdou idaala <abderrahmane@idaala.com>
2023-05-09 20:39:19 -0700

    task[3]

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,11 +16,16 @@ void *exit_97(void)
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
+	char *buffer = malloc(sizeof(char) * 1024);
+	ssize_t r;
 
 	if (argc != 3)
 		exit_97();
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if ((r = read(file_from, buffer, 1024)) != EOF)
+		write(file_to, buffer, r);
+	free(buffer);
 	close(file_from);
 	close(file_to);
 	return (0);
