47d0861720ec0ea1d8a204c2e53bcde2c2a776ba
abdou idaala <abdou@idaala.com>
2023-07-10 13:11:55 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	size_t r, w;
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
