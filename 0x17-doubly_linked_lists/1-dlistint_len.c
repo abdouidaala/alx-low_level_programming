2cf22139b2333bf1c5109af1f42667f4e4729eb2
abdou idaala <abdou@idaala.com>
2023-08-17 16:07:02 +0100

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h)
-    {
-        len++;
-        h = h->next;
-    }
-    return (len);
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
