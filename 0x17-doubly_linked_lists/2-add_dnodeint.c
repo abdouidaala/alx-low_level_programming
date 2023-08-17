7f3d42d087face33b75ab483c8f6de80b5dbeef6
abdou idaala <abdou@idaala.com>
2023-08-17 22:08:12 +0100

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,9 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->prev = NULL;
 	new->n = n;
-	new->next = NULL;
 	new->next = *head;
-	(*head)->prev = new;
 	*head = new;
 
 	return (new);
