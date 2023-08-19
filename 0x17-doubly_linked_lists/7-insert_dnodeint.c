3a7ea9fc45b5d0102c240fa1f7c6afe6d95aa4ff
abdou idaala <abdou@idaala.com>
2023-08-19 21:57:51 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		len++;
 		temp = temp->next;
 	}
-	if (idx == 0)
+	if (idx == 0 || !ptr)
 	{
 		new->next = *h;
 		(*h)->prev = new;
