809e2b282cd6af936437a137585e1a01019d96df
abdou idaala <abdou@idaala.com>
2023-09-07 20:35:37 +0100

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
@@ -16,6 +17,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t) * size);
 	if (!table->array)
 		return (NULL);
+	while (i < size)
+		table->array[i++] = NULL;
 
 	return (table);
 }
