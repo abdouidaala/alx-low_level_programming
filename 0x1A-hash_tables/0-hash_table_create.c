787b8ad782ac29da43e599923fa2f37676fe9522
abdou idaala <abdou@idaala.com>
2023-09-07 19:53:49 +0100

    task-0 + header file + readme file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: The size of the array
+ * Return: a pointer to the newly created hash table, otherwise NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (!table)
+		return (NULL);
+	return (table);
+}
