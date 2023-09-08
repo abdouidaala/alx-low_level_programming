b160991d897e7de151de024a83b867a912f28f07
abdou idaala <abdou@idaala.com>
2023-09-08 20:01:13 +0100

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,15 +11,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *copy_key = NULL;
 	unsigned long int index;
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	hash_node_t *new;
 
-	if (!key || !new)
+	if (!key)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
 	copy_key = strdup(key);
 	index = key_index((const unsigned char *)copy_key, ht->size);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new = ht->array[index];
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	free(copy_key);
 
