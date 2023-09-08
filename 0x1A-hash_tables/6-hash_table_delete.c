cecd60cfbabaef39673b4d2a1a185d1a2658c0f7
abdou idaala <abdou@idaala.com>
2023-09-08 21:39:52 +0100

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,16 +7,16 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0, length = ht->size;
+	unsigned long int i = 0;
 	hash_node_t *ptr;
 
-	while (i < length)
+	while (ht->array[i])
 	{
 		ptr = ht->array[i]->next;
 		free(ht->array[i]->key);
 		free(ht->array[i]->value);
 		free(ht->array[i]);
 		ht->array[i] = ptr;
+		i++;
 	}
-	ht->array = NULL;
 }
