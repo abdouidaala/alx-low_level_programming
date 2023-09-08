9a02e7bd1ca0c26958837539f3a22eca26885410
abdou idaala <abdou@idaala.com>
2023-09-08 11:24:31 +0100

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,11 +8,11 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash = 0;
 	int c;
 
 	for (c = *str; *str; str++)
-		hash = ((hash << 5) + hash) + c;
+		hash += c;
 
 	return (hash);
 }
