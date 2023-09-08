4a20a0325910be742c98f63ea798ca6d1da7ce40
abdou idaala <abdou@idaala.com>
2023-09-08 21:42:01 +0100

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,11 +12,15 @@ void hash_table_delete(hash_table_t *ht)
 
 	while (i < length)
 	{
-		ptr = ht->array[i]->next;
-		free(ht->array[i]->key);
-		free(ht->array[i]->value);
-		free(ht->array[i]);
-		ht->array[i] = ptr;
+		while (ht->array[i])
+		{
+			ptr = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = ptr;
+		}
+
 		i++;
 	}
 }
