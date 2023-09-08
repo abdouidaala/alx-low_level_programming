5e395f8ad433cc9e5f990d110c23d99d37ee9f4f
abdou idaala <abdou@idaala.com>
2023-09-08 20:59:12 +0100

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,6 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	new->next = ht->array[index];
 	ht->array[index] = new;
+	free(copy_key)
 
-	return (1);
+		return (1);
 }
