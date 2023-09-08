628c168fac4a860e16cc416cd15533662b7f4367
abdou idaala <abdou@idaala.com>
2023-09-08 21:24:23 +0100

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: the hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	if (!ht)
+		return;
+}
