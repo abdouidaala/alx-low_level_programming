09c63d6f74bb61e8053b9300fc6f3b3cdda8f51b
abdou idaala <abdou@idaala.com>
2023-09-10 18:31:01 +0100

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <unistd.h>
 
 /**
  * hash_table_print - print a hash table
@@ -7,6 +8,30 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *temp;
+	unsigned long int i = 0, ptr;
+
 	if (!ht)
 		return;
+
+	fprintf(stdout, "{");
+	ptr = 0;
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			if (ptr > 0)
+				fprintf(stdout, ", ");
+			fprintf(stdout, "'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			ptr++;
+		}
+		i++;
+	}
+	fprintf(stdout, "}\n");
 }
+
+/**
+ * {'Betty': 'Cool', 'python': 'awesome', 'Bob': 'and Kris love asm', '98': 'Battery Street', 'N': 'queens', 'c': 'fun', 'Asterix': 'Obelix'}
+ */
