4a0d0101f80ceab625893751e9892677125e91ff
abdou <abdou@idaala.com>
2025-02-16 13:53:07 +0800

    Update task #9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,35 @@
  */
 void times_table(void)
 {
-	int i, j, a;
+	short i;
+	short j;
+	short result;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			a = (i * j);
-			if (a >= 10)
-			{
+			/* Get the result of multiplication */
+			result = i * j;
+
+			/* Print the tens digits if any */
+			if (result >= 10)
+				_putchar((result / 10) + '0');
+			/* Avoid printing space for the first number in the row */
+			else if (j != 0)
 				_putchar(' ');
-				_putchar('0' + (a / 10));
-			}
-			if (a <= 9 && j > 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			_putchar('0' + (a % 10));
+
+			/* Print units digits */
+			_putchar((result % 10) + '0');
+
+			/* Print comma and space, except for the last number in the row */
 			if (j < 9)
 			{
 				_putchar(',');
+				_putchar(' ');
 			}
 		}
+		/* Move to the next line after each row */
 		_putchar('\n');
 	}
 }
