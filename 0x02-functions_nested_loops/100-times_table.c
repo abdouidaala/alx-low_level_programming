2edc55b76e5b5aefa2cbf98d629a8f4c9aa0a01b
abdou <abdou@idaala.com>
2025-02-18 19:50:57 +0800

    Submit task #12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * @brief print_times_table -  print the n times table, starting with 0
+ *
+ * @param n The input number (0 < n < 15)
+ *
+ * @return void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+				_putchar('0'); /* Always starts with '0' */
+			else
+			{
+				/* Formatting for alignment */
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result < 100)
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
