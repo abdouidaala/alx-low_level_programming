56cda454df8c8a17255ee85f4a65b23b8a1e1e4b
abdou <abdou@idaala.com>
2025-02-23 15:39:52 +0800

    Update task #0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - Checks for uppercase character
+ * @brief _isupper - Checks for uppercase character
  *
- * @c: Number to be checked
+ * @param c Number to be checked
  *
- * Return: 1 (Success) OR 0 (Failure)
+ * @return 1 (Success) OR 0 (Failure)
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-	_putchar('\n');
 }
