4c2fa46d328ec652e3da028ec12000e3f47f9ce7
abdou <abdou@idaala.com>
2025-07-04 06:09:39 +0800

    Submit task #9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * print_number - print a number using only the function _putchar().
+ *
+ * @n: The number to print
+ *
+ * Return: void.
+ */
+
+void print_number(int n)
+{
+	bool s = n < 10 && n ? 1 : 0;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n && n < 10)
+	{
+		_putchar((n % 10) + '0');
+		return;
+	}
+	if (!n && !s)
+	{
+		_putchar('0');
+		return;
+	}
+	print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
