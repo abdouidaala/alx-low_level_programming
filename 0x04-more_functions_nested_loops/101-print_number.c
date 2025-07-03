53892d253d0f0e71acb2030924fb971ed385d352
abdou <abdou@idaala.com>
2025-07-03 14:12:29 +0800

    Update task #12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
  * print_number - print a number using only the function _putchar().
@@ -7,18 +8,25 @@
  *
  * Return: void.
  */
+
 void print_number(int n)
 {
-
-	if (!n)
-		return;
-
+	bool s = n < 10 && n ? 1 : 0;
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-
+	if (n && n < 10)
+	{
+		_putchar((n % 10) + '0');
+		return;
+	}
+	if (!n && !s)
+	{
+		_putchar('0');
+		return;
+	}
 	print_number(n / 10);
 	_putchar((n % 10) + '0');
 }
