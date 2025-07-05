72f30821a9ac847d3d09b15673b2d2155ae90568
abdou <abdou@idaala.com>
2025-07-05 12:10:19 +0800

    Submit task #7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,26 @@
 #include <string.h>
 
 /**
+ * _helper - Recursively checks if a substring is a palindrome.
+ *
+ * @s: The input string to check.
+ * @i: The starting index of the substring.
+ * @end: The ending index of the substring.
+ *
+ * Return: 1 if the substring s[i..end] is a palindrome, 0 otherwise.
+ */
+int _helper(char *s, int i, int end)
+{
+	if (i >= end)
+		return 1;
+
+	if (s[i] != s[end])
+		return 0;
+
+	return (_helper(s, i + 1, end - 1));
+}
+
+/**
  * is_palindrome - check if a string is a palindrome.
  *
  * @s: The given string.
@@ -10,15 +30,8 @@
  */
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
+	if (!strlen(s))
+		return (0);
 
-	int i = 0;
-
-	for (; i < len / 2; i++)
-	{
-		if (s[i] != s[len - i - 1])
-			return (0);
-	}
-
-	return (1);
+	return (_helper(s, 0, strlen(s) - 1));
 }
