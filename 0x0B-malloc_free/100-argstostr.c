2b92a5b35b1460a5a3dd209144ed9cd451a722e3
abdou <abdou@idaala.com>
2025-07-05 16:49:59 +0800

    Submit task #5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - Concatenate all the arguments of the program into a one string.
+ *
+ * @ac: The number of arguments.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: pointer to a new string containing all arguments separated by '\n',
+ *         or NULL if ac == 0, av == NULL, or memory allocation fails.
+ *
+ * Description:
+ * This function allocates memory for and creates a new string that contains
+ * each argument from the array 'av', separated by newline characters. The
+ * resulting string is dynamically allocated and should be freed by the caller.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i;
+	size_t j;
+	char *s, *ps;
+
+	if (!ac || !av)
+		return (NULL);
+
+	s = malloc(sizeof(char *) * ac);
+
+	if (!s)
+	{
+		free(s);
+		return (NULL);
+	}
+	ps = s;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < strlen(*(av + i)); j++)
+			*s++ = av[i][j];
+		*s++ = '\n';
+	}
+
+	return (ps);
+}
