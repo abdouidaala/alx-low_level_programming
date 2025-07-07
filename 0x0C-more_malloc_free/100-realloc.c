382692cc02ebc228561ffdf1f4ba5d78d237a2d4
abdou <abdou@idaala.com>
2025-07-07 15:10:07 +0800

    Update task #4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,16 +12,27 @@ The contents will be copied to the newly allocated space, in the range from the 
 If new_size > old_size, the “added” memory should not be initialized
 If new_size == old_size do not do anything and return ptr
 If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
-If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
+If new_size == zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
 Don’t forget to free ptr when it makes sense
  */
-
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size of the new memory block
+ * Return: pointer to the newly allocated memory, or NULL on failure
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
 	if (old_size == new_size)
 		return (ptr);
-	return (NULL);
+	if (!new_size && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 	/*
 	unsigned int i = 0;
 	char *oPtr = ptr;
