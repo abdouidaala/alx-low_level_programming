49dd4dfd5f5f9609dcf1f3a20a928480dec72d26
abdou <abdou@idaala.com>
2025-07-19 07:53:49 +0800

    Submit task #13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+#include <stdbool.h>
+
+/**
+ * free_listint_safe - free a linked list with a loop
+ *
+ * @h: a double pointer to a linked list
+ *
+ * Return: the size of the linked list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *ptr = *h, *slow = *h, *fast = *h;
+	size_t size = 0, i;
+	bool isLoop = 0;
+
+	do
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		size++;
+		if (slow == fast)
+		{
+			isLoop = 1;
+			break;
+		}
+	} while (fast && fast->next);
+	if (!isLoop)
+	{
+		while (*h)
+		{
+			ptr = (*h)->next;
+			free(*h);
+			*h = ptr;
+			size++;
+		}
+	}
+	else
+	{
+		slow = *h;
+		while (slow != fast)
+		{
+			size++;
+			slow = slow->next;
+			fast = fast->next;
+		}
+		ptr = *h;
+		i = 0;
+		while (i < size)
+		{
+			ptr = (*h)->next;
+			free(*h);
+			*h = ptr;
+			i++;
+		}
+		*h = NULL;
+	}
+	return (size);
+}
