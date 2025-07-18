1185df7991b413af5ff68fd1f66585d594b73898
abdou <abdou@idaala.com>
2025-07-18 09:56:11 +0800

    Update task #12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdbool.h>
 
 /**
  * print_listint_safe - print a listint_t linked list.
@@ -9,19 +10,47 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *ptr = head;
+	const listint_t *slow = head;
+	const listint_t *fast = head;
 	size_t size = 0;
+	bool isLoop = 0;
 
-	while (ptr)
+	do
 	{
-		printf("[%p] %d\n", (void *)ptr, ptr->n);
-		if (!ptr->n && ptr->next)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)ptr->next, ptr->next->n);
+			isLoop = 1;
 			break;
 		}
-		ptr = ptr->next;
 		size++;
+	} while (fast && fast->next);
+	slow = head;
+	if (!isLoop)
+		while (slow)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+		}
+	else
+	{
+		while (slow != fast)
+		{
+			size++;
+			slow = slow->next;
+			fast = fast->next;
+		}
+		slow = head;
+		int i = 0;
+		while (i <= size)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			i++;
+		}
+		printf("-> [%p] %d\n", (void *)fast, fast->n);
 	}
+
 	return (size);
 }
